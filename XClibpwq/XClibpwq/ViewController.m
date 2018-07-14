a4050b2ca3cca9ad3df231080ff780fc7075eef8
jufan wang <wangjufan@126.com>
2018-07-14 11:12:31 +0800

    feat: create read

---
+++ XClibpwq/XClibpwq/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  XClibpwq
+//
+//  Created by jufan wang on 2018/7/14.
+//  Copyright © 2018年 jufan wang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
